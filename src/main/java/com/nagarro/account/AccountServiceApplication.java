0e583c098bd84f9c9c80b1fe424ca52aa626c2f4
Manish Gupta <mgupta1@onetrust.com>
2022-02-27 22:00:34 +0530

    Removed maven wrapper

--- src/main/java/com/nagarro/account/AccountServiceApplication.java
+++ src/main/java/com/nagarro/account/AccountServiceApplication.java
@@ -3,6 +3,11 @@ package com.nagarro.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class is to run the main application.
+ * @author Manish Gupta
+ * @version 1.0
+ */
 @SpringBootApplication
 public class AccountServiceApplication {
 
